97bd6b1bfdbf4f3b5b36f3a79f00aa375c1c9c09
MaxHotiuk <hotiukmax@gmail.com>
2023-12-11 16:25:54 +0200

    deleted unused files

--- deliveryworker.py
+++
@@ -1,5 +0,0 @@
-from worker import Worker
-
-class DeliveryWorker(Worker):
-    def __init__(self, name, surname, department, salary):
-        super().__init__(name, surname, department, salary)
--- nondeliveryworker.py
+++
@@ -1,5 +0,0 @@
-from worker import Worker
-
-class NonDeliveryWorker(Worker):
-    def __init__(self, name, surname, department, salary):
-        super().__init__(name, surname, department, salary)
